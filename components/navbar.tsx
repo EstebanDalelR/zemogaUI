2701fe0859485d6c1c6cc8919329c1c735511bd3
estebandalelr <estebandalelr@gmail.com>
2019-11-17 18:48:29 -0500

    Added links on the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,6 @@
+import Link from 'next/link'
 
-export default function Navbar (props) {
+export default function Navbar(props) {
   return (
     <nav>
       <style jsx>{`
@@ -24,6 +25,10 @@ export default function Navbar (props) {
             min-width: 25%;
             list-style-type: none;
           }
+          a {
+            color: white;
+            text-decoration: none;
+          }
           .pageTitle {
             padding: 2em;
           }
@@ -36,23 +41,39 @@ export default function Navbar (props) {
         `}
       </style>
       <div className='navbar'>
-          <span className="pageTitle">
-            Rule of Thumb
+        <span className="pageTitle">
+          Rule of Thumb
           </span>
-          <ul>
-            <li>
-              Past trials
-            </li>
-            <li>
-              How it Works
-            </li>
-            <li>
-              Login / Sign up
-            </li>
-            <li>
-              Search
-            </li>
-          </ul>
+        <ul>
+          <li>
+            <Link href={"pastTrials"}>
+              <a>
+                Past trials
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"howitwors"}>
+              <a>
+                How it Works
+            </a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"login"}>
+              <a>
+                Login / Sign up
+            </a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"search"}>
+              <a>
+                Search
+            </a>
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   )
