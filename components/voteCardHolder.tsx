d9ccd3273441c5f8e232ee10f09142b839cda2f0
estebandalelr <estebandalelr@gmail.com>
2019-11-18 14:51:54 -0500

    Cards now fetch from API

--- components/voteCardHolder.tsx
+++ components/voteCardHolder.tsx
@@ -1,7 +1,7 @@
 import VoteCard from "./voteCard"
-import { useState } from "react"
 
 export default function VoteCardHolder(props) {
+  console.log("resp", props)
 
   return (
     <>
@@ -16,9 +16,12 @@ export default function VoteCardHolder(props) {
         `}
       </style>
       <div className='voteCardHolder'>
-        {props.people.map((person, index) =>
-          <VoteCard votes={person} key={index} id={index} changeVoteCount={props.changeVoteCount}/>
-        )}
+        {props.people.length > 0
+          ? props.people.map((person, index) =>
+            <VoteCard votes={person} key={index} id={index} changeVoteCount={props.changeVoteCount} />
+          )
+          : null
+          }
       </div>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,62 +3,44 @@ import Layout from '../components/layout'
 import ExplanationMessage from '../components/explanationMessage';
 import VoteCard from '../components/voteCard';
 import VoteCardHolder from '../components/voteCardHolder';
-
-let peopleArray = [
-  {
-    name: "Kristina Kichner",
-    time: "1 month",
-    category: "Business",
-    image: "/estebanDalel.jpg",
-    votes: {
-      positive: 1,
-      negative: 2
-    }
-  },
-  {
-    name: "Mark Zuckerberg",
-    time: "1 month",
-    category: "Business",
-    image: "/estebanDalel.jpg",
-    votes: {
-      positive: 2,
-      negative: 2
-    }
-  },
-  {
-    name: "Malala Youzaif",
-    time: "1 month",
-    category: "Business",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Malala_Yousafzai.jpg/220px-Malala_Yousafzai.jpg",
-    votes: {
-      positive: 3,
-      negative: 2
-    }
+import { useState } from "react"
+const Home: NextPage = () => {
+  let [people, setPeople] = useState([])
+  let [hasFetched, setFetched] = useState(false)
+  async function getPeople() {
+    let response = await fetch("/api/db")
+    const myJson = await response.json();
+    return myJson
   }
-]
-let changeVoteCount = (personIndex, value) => {
-  if (value > 0) peopleArray[personIndex].votes.positive++
-  if (value < 0) peopleArray[personIndex].votes.negative++
-}
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <Layout>
-    <section id="homeMainVote">
-      MainPic
-      MainCard
-    </section>
-    <section id="homeSecondaryVotes">
-      <div className="voteCardsHolder">
-        <ExplanationMessage />
-        <VoteCardHolder people={peopleArray} changeVoteCount={changeVoteCount}></VoteCardHolder>
-        Card
-      </div>
-    </section>
-  </Layout>
-);
 
-Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-  return { userAgent };
-};
+  let changeVoteCount = (personIndex, value) => {
+    if (value > 0) people[personIndex].votes.positive++
+    if (value < 0) people[personIndex].votes.negative++
+  }
+  if (!hasFetched) {
+    getPeople()
+      .then(resp => {
+        setPeople(resp.people)
+        setFetched(true)
+      })
+      .catch(err => { console.log(err); return null })
+  }
+  return (
+    <Layout>
+
+      <section id="homeMainVote">
+        MainPic
+        MainCard
+    </section>
+      <section id="homeSecondaryVotes">
+        <div className="voteCardsHolder">
+          <ExplanationMessage />
+          <VoteCardHolder people={people} changeVoteCount={changeVoteCount}></VoteCardHolder>
+          Card
+      </div>
+      </section>
+    </Layout>
+  );
+}
 
 export default Home;
