7665838a8773304ebb37bfa534648272203c7713
estebandalelr <estebandalelr@gmail.com>
2019-11-18 12:56:03 -0500

    People getter working

--- pages/api/db.js
+++ pages/api/db.js
@@ -3,15 +3,24 @@ import { throws } from 'assert'
 export default (req, res) => {
   const people = []
   
-  const table = base('Locations')
+  const table = base('Cards')
 
   try {
     table.select({
       view: 'Grid view'
     }).all().then(records => {
       records.forEach(function (record) {
-        let location = { city: record.get('City'), name: record.get('Name'), times: record.get('Business Hours').split(','), image: (record.get('Image') ? record.get('Image')[0].url : undefined) }
-        people.push(location)
+        let person = { 
+          category: record.get('Category'), 
+          name: record.get('Name'), 
+          postedAt: record.get('PostedAt'), 
+          ImageURL: record.get('ImageURL'), 
+          votes: {
+            positive: record.get('Positive'),
+            negative: record.get('Negative')
+          }
+        }
+        people.push(person)
       })
       res.setHeader('Content-Type', 'application/json')
       res.statusCode = 200
