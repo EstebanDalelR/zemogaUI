41b673b697070e88787a99113313fef5fc724105
estebandalelr <estebandalelr@gmail.com>
2019-11-19 15:52:58 -0500

    Added fixture to cypress test

---
+++ cypress/integration/zemoga/z-navigation.spec.js
@@ -0,0 +1,21 @@
+describe("Opens the page", ()=>{
+  let baseURL = 'http://localhost:3000'
+  it("Uses the app logging in with a preset account", ()=>{
+    cy.visit(baseURL)
+    /* Set server and fixtures, as to respond with the people to vote */
+    cy.server()
+    cy.fixture('people.json').as('peopleJSON')
+    cy.route("GET", '/api/db', '@peopleJSON').as("getPeople")
+    
+    cy.contains("Login").click() 
+    cy.url().should("include", "/login")
+    cy.get("#name").type("alguien")
+    cy.get("#password").type("dasda")
+    cy.get("#submit").click()
+    cy.visit(baseURL)
+    cy.wait(500)
+    cy.get(".voteNegative").first().click()
+    cy.get(".castVote").first().click()
+    cy.contains("Vote again") 
+  })
+})
