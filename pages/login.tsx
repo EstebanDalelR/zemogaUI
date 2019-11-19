6ff6b9a1335d64b418e1d1f1ec6d88736d8fdc02
estebandalelr <estebandalelr@gmail.com>
2019-11-19 10:55:57 -0500

    UI now handles error in username and password

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,11 +6,76 @@ const Login: NextPage = () => {
   let [name, setName] = useState("")
   let [password, setPassword] = useState("")
   let [hasFetched, setFetched] = useState(false)
-
-  let handleSubmit= (e) =>{
+  let [userStatus, setUsetStatus] = useState("")
+  async function sendUser() {
+    let data = {
+      "username": name
+    }
+    let resp = await fetch(
+      "/api/login",
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+    )
+    const myJson = await resp.json();
+    return myJson
+  }
+  let handleSubmit = (e) => {
     e.preventDefault()
-    let data ={ name, password }
-    console.log(data)
+    sendUser()
+      .then(userpass => {
+        setFetched(true)
+        if (userpass.user.length > 0) {
+          if (userpass.user[0].password === password) {
+            setUsetStatus("logged")
+          } else {
+            setUsetStatus("wrongPass")
+          }
+        } else {
+          setUsetStatus("notFound")
+        }
+      })
+  }
+  let handleLogin = () => {
+    let ui = <></>
+    switch (userStatus) {
+      case "logged":
+        ui = <div>
+          <Link href="/">
+            <a>
+              <p>
+                Logged in, you may now vote in the home
+                      </p>
+              <p>
+                {name}
+              </p>
+            </a>
+          </Link>
+        </div>
+        break;
+      case "wrongPass":
+        ui = <div>
+          <p style={{ color: "red" }}>Wrong password</p>
+          <button onClick={()=>setFetched(false)}>Try again</button>
+        </div>
+        break;
+      case "notFound":
+        ui = <div>
+          <p style={{ color: "red" }}>User not Found</p>
+          <button onClick={()=>setFetched(false)}>Try again</button>
+        </div>
+        break;
+      default:
+        break;
+    }
+    console.log(userStatus)
+    console.log(hasFetched)
+    console.log(ui)
+    return ui
   }
   return (
     <Layout>
@@ -20,43 +85,49 @@ const Login: NextPage = () => {
           }
         `}
       </style>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <h3>Login</h3>
-        <div>
-          <label htmlFor="name">Username:</label>
-          <input
-            type="text"
-            id="name"
-            required
-            name="name"
-            value={name}
-            onChange={(ev)=>setName(ev.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            required
-            value={password}
-            minLength={4}
-            onChange={(ev)=>setPassword(ev.target.value)}
-            name="password"
-          />
-        </div>
-        <div>
-          <label htmlFor="submit">Login:</label>
-          <input type="submit" id="submit" name="submit" />
-        </div>
-      </form>
-      <div>
-        <Link href="/signup">
-          <a>
-            Would you rather create an account?
+      {hasFetched
+        ? <>{handleLogin()}</>
+        : <>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <h3>Login</h3>
+            <div>
+              <label htmlFor="name">Username:</label>
+              <input
+                type="text"
+                id="name"
+                required
+                name="name"
+                value={name}
+                onChange={(ev) => setName(ev.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                required
+                value={password}
+                minLength={4}
+                onChange={(ev) => setPassword(ev.target.value)}
+                name="password"
+              />
+            </div>
+            <div>
+              <label htmlFor="submit">Login:</label>
+              <input type="submit" id="submit" name="submit" />
+            </div>
+          </form>
+          <div>
+            <Link href="/signup">
+              <a>
+                Would you rather create an account?
           </a>
-        </Link>
-      </div>
+            </Link>
+          </div>
+        </>
+      }
+
     </Layout>
   );
 }
