5f16b25b524d954d12df2e1c6036831c5e29a840
estebandalelr <estebandalelr@gmail.com>
2019-11-19 16:16:46 -0500

    Added a test to try to ue the app not logged

--- cypress/integration/zemoga/z-navigation.spec.js
+++ cypress/integration/zemoga/z-navigation.spec.js
@@ -6,7 +6,7 @@ describe("Opens the page", ()=>{
     cy.server()
     cy.fixture('people.json').as('peopleJSON')
     cy.route("GET", '/api/db', '@peopleJSON').as("getPeople")
-    
+
     cy.contains("Login").click() 
     cy.url().should("include", "/login")
     cy.get("#name").type("alguien")
@@ -17,5 +17,20 @@ describe("Opens the page", ()=>{
     cy.get(".voteNegative").first().click()
     cy.get(".castVote").first().click()
     cy.contains("Vote again") 
+  }) 
+  it("Tries to use the app without logging in", ()=>{
+    const stub = cy.stub()
+    cy.on('window:alert', stub)
+    cy.visit(baseURL)
+    /* Set server and fixtures, as to respond with the people to vote */
+    cy.server()
+    cy.fixture('people.json').as('peopleJSON')
+    cy.route("GET", '/api/db', '@peopleJSON').as("getPeople")
+    cy.wait(500)
+    cy.get(".voteNegative").first().click()
+    cy.get(".castVote").first().click()
+    .then(() => {
+      expect(stub.getCall(0)).to.be.calledWith('You must be logged in to Vote')
+    })
   })
 })
